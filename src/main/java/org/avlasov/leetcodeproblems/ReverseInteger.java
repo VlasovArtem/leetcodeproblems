707d4676b2ba17ce77cde647363846745c7f4253
Vlasov Artem <avlasov@selectica.com>
2018-06-21 13:14:51 +0300

    Add Reverse Integer problem class and test

---
+++ src/main/java/org/avlasov/leetcodeproblems/ReverseInteger.java
@@ -0,0 +1,19 @@
+package org.avlasov.leetcodeproblems;
+
+/**
+ * Created By artemvlasov on 21/06/2018
+ **/
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        int sum = 0;
+        while (x != 0) {
+            if (Integer.MAX_VALUE < ((long) sum) * 10 || Integer.MIN_VALUE > ((long) sum) * 10) return 0;
+            sum *= 10;
+            sum += x % 10;
+            x = x / 10;
+        }
+        return sum;
+    }
+
+}
---
+++ src/test/java/org/avlasov/leetcodeproblems/ReverseIntegerTest.java
@@ -0,0 +1,43 @@
+package org.avlasov.leetcodeproblems;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created By artemvlasov on 21/06/2018
+ **/
+public class ReverseIntegerTest {
+
+    private ReverseInteger reverseInteger;
+
+    public ReverseIntegerTest() {
+        reverseInteger = new ReverseInteger();
+    }
+
+    @Test
+    public void reverse_WithValidData1_ReturnInt() {
+        assertEquals(321, reverseInteger.reverse(123));
+    }
+
+    @Test
+    public void reverse_WithValidData2_ReturnInt() {
+        assertEquals(-123, reverseInteger.reverse(-321));
+    }
+
+    @Test
+    public void reverse_WithValidData3_ReturnInt() {
+        assertEquals(21, reverseInteger.reverse(120));
+    }
+
+    @Test
+    public void reverse_WithMaxValue_ReturnZero() {
+        assertEquals(0, reverseInteger.reverse(1534236469));
+    }
+
+    @Test
+    public void reverse_WithMinValue_ReturnZero() {
+        assertEquals(0, reverseInteger.reverse(-2147483648));
+    }
+
+}
