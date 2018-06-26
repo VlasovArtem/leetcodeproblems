a1c8197f54ff255b1047657a356143a7c4980e36
Vlasov Artem <avlasov@selectica.com>
2018-06-26 15:22:05 +0300

    Update AddTwoNumbers test solution

--- src/test/java/org/avlasov/leetcodeproblems/AddTwoNumbersTest.java
+++ src/test/java/org/avlasov/leetcodeproblems/AddTwoNumbersTest.java
@@ -1,7 +1,9 @@
 package org.avlasov.leetcodeproblems;
 
+import org.avlasov.leetcodeproblems.data.ListNode;
 import org.junit.Test;
 
+import static org.avlasov.leetcodeproblems.utils.ListNodeUtils.createListNode;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -18,42 +20,40 @@ public class AddTwoNumbersTest {
 
     @Test
     public void addTwoNumbers_WithValidData_ReturnListNode() {
-        AddTwoNumbers.ListNode listNode = addTwoNumbers.addTwoNumbers(addTwoNumbers.createListNode(2, 4, 3), addTwoNumbers.createListNode(5, 6, 4));
+        ListNode listNode = addTwoNumbers.addTwoNumbers(createListNode(2, 4, 3), createListNode(5, 6, 4));
         assertNotNull(listNode);
-        assertEquals(addTwoNumbers.createListNode(7, 0, 8), listNode);
+        assertEquals(createListNode(7, 0, 8), listNode);
     }
 
     @Test
     public void addTwoNumbers_WithZero_ReturnListNode() {
-        AddTwoNumbers.ListNode listNode = addTwoNumbers.addTwoNumbers(addTwoNumbers.new ListNode(0), addTwoNumbers.new ListNode(0));
+        ListNode listNode = addTwoNumbers.addTwoNumbers(new ListNode(0), new ListNode(0));
         assertNotNull(listNode);
-        assertEquals(addTwoNumbers.new ListNode(0), listNode);
+        assertEquals(new ListNode(0), listNode);
     }
 
     @Test
     public void addTwoNumbers_WithNotMatchingSizeNumbers_ReturnListNode() {
-        AddTwoNumbers.ListNode listNode = addTwoNumbers.addTwoNumbers(addTwoNumbers.createListNode(6, 5, 3, 2), addTwoNumbers.createListNode(8, 3, 6));
+        ListNode listNode = addTwoNumbers.addTwoNumbers(createListNode(6, 5, 3, 2), createListNode(8, 3, 6));
         assertNotNull(listNode);
-        assertEquals(addTwoNumbers.createListNode(4, 9, 9, 2), listNode);
+        assertEquals(createListNode(4, 9, 9, 2), listNode);
     }
 
     @Test
-    public void addTwoNumbers_WithSingleNumberExtendedToAddtionalNumber_ReturnListNode() {
-        AddTwoNumbers.ListNode listNode = addTwoNumbers.addTwoNumbers(addTwoNumbers.createListNode(5), addTwoNumbers.createListNode(5));
+    public void addTwoNumbers_WithSingleNumberExtendedToAdditionalNumber_ReturnListNode() {
+        ListNode listNode = addTwoNumbers.addTwoNumbers(createListNode(5), createListNode(5));
         assertNotNull(listNode);
-        assertEquals(addTwoNumbers.createListNode(0, 1), listNode);
+        assertEquals(createListNode(0, 1), listNode);
     }
-
-
 
     @Test(expected = IllegalArgumentException.class)
     public void addTwoNumbers_WithNullFirstArgument_ThrowsIllegalArgumentException() {
-        addTwoNumbers.addTwoNumbers(null, addTwoNumbers.createListNode(2));
+        addTwoNumbers.addTwoNumbers(null, createListNode(2));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void addTwoNumbers_WithNullSecondArgument_ThrowsIllegalArgumentException() {
-        addTwoNumbers.addTwoNumbers(addTwoNumbers.createListNode(2), null);
+        addTwoNumbers.addTwoNumbers(createListNode(2), null);
     }
 
 }
